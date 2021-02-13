405d9f8bfacb06609a8d67ba320cb5eca96f325e
Daniela Velez <danielavelez1201@gmail.com>
2021-02-13 00:47:41 -0500

    Open AI API call

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+require('dotenv').config()
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/ping', (req, res) => {
-    console.log("in backend")
     return res.send('pong')
 })
 
--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -3,11 +3,29 @@ import React, { useState, useEffect } from 'react';
 
 function Home() {
     const [output, setOutput] = useState("");
+    const [openAI, setOpenAI] = useState({});
+
+    const options = {
+        method: 'GET',
+        url: 'https://api.openai.com/v1/engines',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: process.env.REACT_APP_API_KEY
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+      }).catch(function (error) {
+        console.error(error);
+      });
 
     useEffect(async () => {
         const result = await axios.get("http://localhost:8080/ping")
         setOutput(result.data);
-        console.log(output)
+        console.log(process.env.REACT_APP_API_KEY)
+        
+
     })
 
     return (
